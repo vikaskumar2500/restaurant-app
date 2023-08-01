42f61db02356926fc5237e77c4b8c342ccf36368
vikaskumar2500 <vikas.nits8084@gamil.com>
2023-08-01 23:55:18 +0530

    added summary component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import Summary from './components/Summary/Summary';
 
 const App = ()=> {
   return <React.Fragment>
     <Header/>
+    <Summary/>
   </React.Fragment>
 }
 
---
+++ src/components/Summary/Summary.jsx
@@ -0,0 +1,19 @@
+import "./Summary.css";
+
+const Summary = () => {
+  return (
+    <div className="summary">
+      <h1>Delicious Food, Delivered To You</h1>
+      <span className="text1">
+        Choose your favourite meal from our broad selection of available meals
+        and enjoy a delicious lunch or dinner at home
+      </span>
+      <span className="text2">
+        All our meals are cooked with high-quality ingredients, just in time and
+        of course by experienced chefs
+      </span>
+    </div>
+  );
+};
+
+export default Summary;
